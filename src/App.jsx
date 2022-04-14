d89d3fffcbf1ff7e54dd2c564b12d22d04f87033
eddybruv <edwinajong@gmail.com>
2022-04-14 20:59:31 +0100

    currently rendering card, todo: add routes

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import SignUp from "./Components/SignUp";
+import Login from "./Components/Login";
+import Card from "./Components/Card";
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <SignUp /> */}
+      {/* <Login /> */}
+      <Card />
+    </div>
+  );
+}
+
+export default App;
