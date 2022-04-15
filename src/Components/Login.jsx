9ce1ebd53f4a4cf2744b39b309e31f168ae933b5
eddybruv <edwinajong@gmail.com>
2022-04-15 22:18:14 +0100

    todo: control widths

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,17 +1,25 @@
 import React from "react";
+import {useNavigate} from 'react-router-dom';
 
 function Login() {
+  let navigate = useNavigate();
   return (
     <div className="login">
       <div className="login-details">
         <h2 className="login-account">Login</h2>
         <form action="" className="login-form">
           <label htmlFor="phone-number">Phone Number</label> <br />
-          <input type="number" id="phone-number" /> <br />
-          
+          <input type="number" name="phone-number" /> <br />
           <label htmlFor="password">Password</label> <br />
-          <input type="password" id="password" />
-          <button className="login-button">Sign Up</button>
+          <input type="password" name="password" />
+          <button
+            className="login-button"
+            onClick={() => {
+              navigate("/timeline");
+            }}
+          >
+            Login
+          </button>
         </form>
       </div>
     </div>
