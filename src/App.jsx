29473d142eb2f42edbfaa979ef319a55999d1175
eddybruv <edwinajong@gmail.com>
2022-04-15 22:19:03 +0100

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import "./App.css";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import SignUp from "./Components/SignUp";
 import Login from "./Components/Login";
-import Card from "./Components/Card";
+import Home from "./Components/Home";
+import Timeline from "./Components/Timeline";
+import DailyFeed from './Components/DailyFeed';
+import NewPost from "./Components/NewPost";
+import Error from "./Components/Error";
 
 
 function App() {
   return (
-    <div className="App">
-      {/* <SignUp /> */}
-      {/* <Login /> */}
-      <Card />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/timeline" element={<Timeline />} />
+        <Route path="/feed" element={<DailyFeed />} />
+        <Route path="/new" element={<NewPost />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
