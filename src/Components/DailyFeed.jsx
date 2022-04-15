3f662c39e25f4147cd1e5ddacfb29fc8a9882eab
eddybruv <edwinajong@gmail.com>
2022-04-15 22:18:53 +0100

    feed page

---
+++ src/Components/DailyFeed.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import UserPanel from './UserPanel';
+import Card from './Card';
+
+function Comments() {
+  return (
+    <div className="timeline-page">
+      <div className="timeline-container">
+        <UserPanel />
+        <div className="feed">
+          <p className="feed-text">Daily Feed</p>
+          <Card />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Comments
