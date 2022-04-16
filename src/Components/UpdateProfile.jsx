dd1356b9d6079a0e319f66e1ce1e01f1620b9301
eddybruv <edwinajong@gmail.com>
2022-04-16 12:26:27 +0100

    settings section

---
+++ src/Components/UpdateProfile.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import UserPanel from './UserPanel';
+
+function UpdateProfile() {
+  return (
+    <div className="timeline-page">
+      <div className="timeline-container">
+        <UserPanel />
+        <div className="feed">
+          <p className="feed-text">Update Your Account Details</p>
+          <div className="present-details">
+            <h3>Current Information</h3>
+            <p className="cur-name">
+              Name : <span>Eddy Bruv</span>
+            </p>
+            <p className="password">
+              Password : <span>bimamiA4</span>
+            </p>
+            <p className="cur-number">
+              Number : <span>673928764</span>
+            </p>
+          </div>
+          <div className="new-details">
+            <h3>New Information</h3>
+            <form action="/update" method='post'>
+              <input type="text" name="name" placeholder="Enter Name" /> <br />
+              <input type="tel" name="number" placeholder="Enter Number" /> <br />
+              <input type="password" name="password" placeholder="Enter Password" /> <br />
+              <button type='submit'>Submit</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UpdateProfile
