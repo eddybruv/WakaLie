a41b7c0e1a4621af80f9600426bc9c5ec4c6c1e9
eddybruv <edwinajong@gmail.com>
2022-04-16 22:05:36 +0100

    dynamically add card details

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,30 +1,21 @@
-import React, {useState} from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 
-function Card() {
-  /* const [showOptions, setShowOptions] = useState(false);
-
-  const handleOptions = (e) => {
-    if(!showOptions) {
-      e.className.add('me')
-      setShowOptions(true);
-    }
-  } */
+function Card({ name, message, time }) {
   return (
     <div className="personal-card">
       <img src="https://avatars.githubusercontent.com/u/80322163?v=4" alt="" />
       <div className="name-time-message">
-        <p className="user-name">Eddy Bruv</p>
-        <p className="time">01:30 pm</p>
-        <p className="message">
-          Hello everyone, I just joined WakaLie and I kinda find it cool as I
-          get to speak freely and safely.
+        <p className="user-name">{name}</p>
+        <p className="time">
+          {time.split("T")[1].slice(0, 5)} {time.split("T")[0]}
         </p>
+        <p className="message">{message}</p>
       </div>
       <FontAwesomeIcon className="ellipsis" icon={faEllipsisVertical} />
     </div>
   );
 }
 
-export default Card
+export default Card;
