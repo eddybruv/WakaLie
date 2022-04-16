6c411f90f143934470a738251b3dddd27b59d594
eddybruv <edwinajong@gmail.com>
2022-04-16 22:04:39 +0100

    fetch all posts from the database

--- src/Components/DailyFeed.jsx
+++ src/Components/DailyFeed.jsx
@@ -1,30 +1,47 @@
-import React from 'react';
-import UserPanel from './UserPanel';
-import Card from './Card';
+import React, { useEffect, useState } from "react";
+import UserPanel from "./UserPanel";
+import Card from "./Card";
+import axios from "axios";
 
 function Comments() {
+  const [comments, setComments] = useState(null);
+
+  const fetchServer = async () => {
+    const result = await axios
+      .get("http://localhost:5000/api/post/get_post")
+      // .then((data) => console.log(data.data));
+      console.log(result.data[1].user_id);
+    setComments(result.data);
+  };
+
+  useEffect(() => {
+    // console.log(comments);
+  }, [comments]);
+
+  useEffect(() => {
+    fetchServer();
+  }, []);
+
   return (
     <div className="timeline-page">
       <div className="timeline-container">
         <UserPanel />
         <div className="feed">
           <p className="feed-text">Daily Feed</p>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {comments !== null &&
+            comments
+              .map((comment, index) => (
+                <Card key={index}
+                  message={comment.message}
+                  name={comment.user_id.name}
+                  time={comment.createdAt}
+                />
+              ))
+              .reverse()}
         </div>
       </div>
     </div>
   );
 }
 
-export default Comments
+export default Comments;
