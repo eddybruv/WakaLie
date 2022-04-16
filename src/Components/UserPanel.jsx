576f482367fcbe766c57030d0a06351fc01d91d9
eddybruv <edwinajong@gmail.com>
2022-04-16 12:26:18 +0100

    added logout route

--- src/Components/UserPanel.jsx
+++ src/Components/UserPanel.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function UserPanel() {
+  // let pathname = window?.location.pathname;
+  /* let curPath = window?.location.pathname;
+
+  const handleSelect = (e) => {
+    let isActive = false;
+    let susPath = e.target.href;
+    if (susPath.includes(curPath) !== -1)
+      isActive = true
+  }; */
+
   return (
     <div className="user-panel">
       <img
@@ -10,9 +20,10 @@ function UserPanel() {
         className="user-img"
       />
       <p className="username">Eddy Bruv</p>
+      <div className="line"></div>
       <ul>
         <li>
-          <Link to="/feed">Daily Feed</Link>
+          <Link to="/feed" onClick={handleSelect}>Daily Feed</Link>
         </li>
         <li>
           <Link to="/timeline">Timeline</Link>
@@ -21,12 +32,12 @@ function UserPanel() {
           <Link to="/new">New Post</Link>
         </li>
         <li>
-          <Link to="">Profile</Link>
+          <Link to="/update">Profile</Link>
         </li>
       </ul>
 
       <p className="logout">
-        <Link to="">Logout</Link>
+        <Link to="/home">Logout</Link>
       </p>
     </div>
   );
