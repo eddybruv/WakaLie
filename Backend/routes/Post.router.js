11b5152f375f54bab4b5a84c703e648473d64fae
eddybruv <edwinajong@gmail.com>
2022-04-17 20:36:44 +0100

    post routes

---
+++ Backend/routes/Post.router.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const PostModel = require("../models/Post.model");
+
+const router = express.Router();
+
+// create post
+router.post("/create_post", (req, res) => {
+  const { user_id, message } = req.body;
+  const post = new PostModel({ user_id: user_id, message: message });
+  post.save();
+
+  res.json({ message: "Post created", data: post });
+});
+
+// see post
+router.get("/get_post", async (req, res) => {
+  const posts = await PostModel.find({})
+  .populate({path: 'user_id', select: '-password'});
+  res.json(posts);
+});
+
+// update post
+router.put("/update_post", async (req, res) => {
+  const { _id } = req.body;
+  const updatedPost =  await PostModel.findOneAndUpdate({ _id: _id }, {...req.body});
+
+  res.json({ message: "Post updated", data: updatedPost });
+});
+
+// delete post
+router.delete("/delete_post", async (req, res) => {
+  const {_id} = req.body;
+  const deletePost = await PostModel.findOneAndDelete({_id: _id}, {...req.body});
+  res.json({ message: "Post deleted", data: deletePost });
+})
+module.exports = router;
