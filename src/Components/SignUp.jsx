d7364239068cd5cb2f6cf8c04aed85d8ccdceb16
eddybruv <edwinajong@gmail.com>
2022-04-25 20:41:27 +0100

    added profile image

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -2,16 +2,20 @@ import axios from "axios";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+const preset = "ecy9pmhx";
+
 function SignUp() {
   let navigate = useNavigate();
 
   const [user, setUser] = useState({
     name: "",
-    number: undefined,
+    number: "",
     password: "",
     dob: "",
     gender: "",
   });
+
+  const [image, setImage] = useState("");
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -21,13 +25,38 @@ function SignUp() {
     });
   };
 
-  const submit = async () => {
-    console.log(user);
-    let response = await axios.post(
-      "http://localhost:5000/api/user/create_account",
-      user
-    );
+  const handleImage = (e) => {
+    setImage(e.target.files[0]);
+    console.log(e.target.files);
+    console.log({ ...user, image });
+  };
 
+  const submit = async () => {
+    const formData = new FormData();
+    formData.append("file", image);
+    formData.append("upload_preset", preset);
+
+    const res = await axios
+      .post("https://api.cloudinary.com/v1_1/wakalie/image/upload", formData)
+      .then((data) => {
+        return data.data.secure_url;
+      })
+      .catch(() => {
+        console.log("Doesn't work");
+      });
+    console.log(res);
+    const imageUrl = res;
+
+    const finalUser = { ...user, imageUrl };
+    console.log(finalUser);
+    let response = await axios
+      .post("http://localhost:5000/api/user/create_account", finalUser)
+      .then((data) => {
+        console.log(data);
+      })
+      .catch(() => console.log("nothing works"));
+    
+    console.log(response);
     if (response.status === 200) {
       localStorage.setItem("currentUser", JSON.stringify(user));
       navigate("/feed");
@@ -41,6 +70,8 @@ function SignUp() {
       <div className="signup-details">
         <h2 className="create-account">Create Account</h2>
         <form action="" className="signup-form">
+          <label htmlFor="image">Profile Picture</label>
+          <input type="file" name="image" onChange={handleImage} />
           <label htmlFor="name">Name</label> <br />
           <input
             type="text"
