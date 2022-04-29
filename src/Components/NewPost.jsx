8bf71255e68c2380fb8b3806fd006aa5283f64e5
eddybruv <edwinajong@gmail.com>
2022-04-29 21:09:33 +0100

    implemented proper posting by population user id

--- src/Components/NewPost.jsx
+++ src/Components/NewPost.jsx
@@ -6,21 +6,32 @@ import axios from "axios";
 function NewPost() {
   let navigate = useNavigate();
   const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    localStorage.setItem("user_id", "6255ba50e22e15757a61a147");
-  }, []);
-
-  const handleChange = (event) => {
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+  
+  const handleChange = (event) => { 
     setMessage(event.target.value);
   }
 
+  const [user, setUser] = useState(null);
+
+  const fetchServer = async () => {
+    const result = await axios.post(
+      "http://localhost:5000/api/user/get_user",
+      {name: currentUser.name}
+    );
+    setUser(result.data);
+  };
+
+  useEffect(() => {
+    fetchServer();
+  }, [user]);
+
   const submit = async() => {
     console.log(message);
-    const response = await axios.post('http://localhost:5000/api/post/create_post', {message, user_id: localStorage.getItem('user_id')});
+    const response = await axios.post('http://localhost:5000/api/post/create_post', {message, user_id: user._id});
 
     if (response.status === 200) {
-      console.log('post sent')
+      alert('post sent')
       navigate('/feed');
     } else {
       alert('Post not sent');
