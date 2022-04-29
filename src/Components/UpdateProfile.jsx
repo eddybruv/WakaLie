52c584449dbe3d2edcb99b3aae2166722fde32ad
eddybruv <edwinajong@gmail.com>
2022-04-29 21:08:42 +0100

    added user information to be displayed

--- src/Components/UpdateProfile.jsx
+++ src/Components/UpdateProfile.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import UserPanel from './UserPanel';
 
 function UpdateProfile() {
+
+  const user = JSON.parse(localStorage.getItem("currentUser"));
+
   return (
     <div className="timeline-page">
       <div className="timeline-container">
@@ -11,22 +14,36 @@ function UpdateProfile() {
           <div className="present-details">
             <h3>Current Information</h3>
             <p className="cur-name">
-              Name : <span>Eddy Bruv</span>
+              Name : <span>{user.name}</span>
             </p>
             <p className="password">
-              Password : <span>bimamiA4</span>
+              Password : <span>{user.password}</span>
             </p>
             <p className="cur-email">
-              Email : <span>example@gmail.com</span>
+              Email : <span>{user.email}</span>
+            </p>
+            <p className="cur-dob">
+              DoB : <span>{user.dob}</span>
             </p>
           </div>
           <div className="new-details">
             <h3>New Information</h3>
-            <form action="/update" method='post'>
+            <form action="/update" method="post">
               <input type="text" name="name" placeholder="Enter Name" /> <br />
               <input type="tel" name="email" placeholder="Enter email" /> <br />
-              <input type="password" name="password" placeholder="Enter Password" /> <br />
-              <button type='submit'>Submit</button>
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter Password"
+              />{" "}
+              <br />
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter Email"
+              />{" "}
+              <br />
+              <button type="submit">Submit</button>
             </form>
           </div>
         </div>
