c3c089689933601277490c4fec739af6a8df84bc
eddybruv <edwinajong@gmail.com>
2022-04-29 21:08:03 +0100

    cleared localstorage on logout

--- src/Components/UserPanel.jsx
+++ src/Components/UserPanel.jsx
@@ -1,22 +1,15 @@
-import { clear } from "@testing-library/user-event/dist/clear";
 import React from "react";
-import { Link, } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function UserPanel() {
-
-  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
-  console.log(currentUser);
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));  
 
   const onLogOut = () => {
-    clear();
+    window.localStorage.clear();
   };
   return (
     <div className="user-panel">
-      <img
-        src={currentUser.imageUrl}
-        alt=""
-        className="user-img"
-      />
+      <img src={currentUser.imageUrl} alt="" className="user-img" />
       <p className="username">{currentUser.name}</p>
       <div className="line"></div>
       <ul>
@@ -35,7 +28,9 @@ function UserPanel() {
       </ul>
 
       <p className="logout">
-        <Link to="/home" onClick={onLogOut}>Logout</Link>
+        <Link to="/home" onClick={onLogOut}>
+          Logout
+        </Link>
       </p>
     </div>
   );
