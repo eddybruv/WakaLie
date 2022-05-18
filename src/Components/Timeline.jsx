7d2e6ca93c912e1a3842f92a73ed31d533540283
eddybruv <edwinajong@gmail.com>
2022-05-18 18:01:47 +0100

    timeline working well now

--- src/Components/Timeline.jsx
+++ src/Components/Timeline.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UserPanel from "./UserPanel";
-import Card from './Card'
+import Card from "./Card";
+import axios from "axios";
 
 function Timeline() {
+  const [posts, setPosts] = useState(null);
+
+  const user = JSON.parse(localStorage.getItem("currentUser"));
+
+  useEffect(() => {
+    const fetchServer = async () => {
+      const result = await axios
+        .post("http://localhost:5000/api/post/user_posts", { _id: user._id })
+        .then((data) => data.data)
+        .catch(() => console.log("Doesnt work"));
+      setPosts(result);
+    };
+
+    fetchServer();
+  }, []);
+
   return (
     <div className="timeline-page">
       <div className="timeline-container">
-        <UserPanel/>
+        <UserPanel />
         <div className="feed">
           <p className="feed-text">Your Posts</p>
-          <Card/>
+          {posts?.map((post, index) => {
+            return <Card key={index} image={post.user_id.imageUrl} name={post.user_id.name} message={post.message} time={post.createdAt}/>;
+          })}
         </div>
       </div>
     </div>
