cb26ffd97f540c9712e774f80a3f6f3cddfbbd55
eddybruv <edwinajong@gmail.com>
2022-05-19 16:13:28 +0100

    removed number path

--- models/User.model.js
+++ models/User.model.js
@@ -5,7 +5,6 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   gender: { type: String },
   dob: { type: Date, required: true },
-  number: { type: Number, required: true },
   password: { type: String, required: true },
   imageUrl: { type: String, required: true },
 });
